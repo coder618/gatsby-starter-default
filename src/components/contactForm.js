b4190a68c918baa1ae563332140927b115db8ec6
Ahadul islam <iamahadul@gmail.com>
2023-04-22 22:20:05 +0600

    contact form trial 1

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,26 +1,53 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-const ContactForm = ({ siteTitle }) => (
-  <div className="contact-form">
-    <div className="content">
-      <h2>Contact Us</h2>
-      <div className="form-group">
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+const ContactForm = () => {
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    const myForm = event.target
+    const formData = new FormData(myForm)
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log("/thank-you/"))
+      .catch(error => alert(error))
+  }
+
+  return (
+    <form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      onSubmit={handleSubmit}
+    >
+      <div className="contact-form">
+        <div className="content">
+          <h2>Contact Us</h2>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea id="message" name="message"></textarea>
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              CONTACT US
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="form-group">
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="message">Message</label>
-        <textarea id="message" name="message"></textarea>
-      </div>
-      <div className="form-group">
-        <button className="btn btn-primary">CONTACT US</button>
-      </div>
-    </div>
-  </div>
-)
+    </form>
+  )
+}
+
 export default ContactForm
