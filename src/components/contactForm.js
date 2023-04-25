056106e1d980b41ff1cc1dd30ca89885e4ff5aa0
Ahadul islam <iamahadul@gmail.com>
2023-04-25 13:41:34 +0600

    manual contact form message added

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,9 +1,14 @@
 import * as React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
+import { useState } from "react"
 
 const ContactForm = () => {
+  const [submitted, setSubmitted] = useState(false)
+  const [responseMessage, setResponseMessage] = useState("")
+
   const handleSubmit = event => {
     event.preventDefault()
+    setSubmitted(true)
 
     const myForm = event.target
     const formData = new FormData(myForm)
@@ -13,8 +18,16 @@ const ContactForm = () => {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     })
-      .then(() => console.log("/thank-you/"))
-      .catch(error => alert(error))
+      .then(() => {
+        setSubmitted(false)
+        setResponseMessage("We Have received your message")
+      })
+      .catch(error => {
+        setSubmitted(false)
+        setResponseMessage(
+          "Something is wrong please contact us at testing@gmail.com "
+        )
+      })
   }
   return (
     <form
@@ -29,18 +42,27 @@ const ContactForm = () => {
           <h2>Contact Us</h2>
           <div className="form-group">
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" />
+            <input type="text" id="name" name="name" required />
           </div>
           <div className="form-group">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" />
+            <input type="email" id="email" name="email" required />
           </div>
           <div className="form-group">
             <label htmlFor="message">Message</label>
-            <textarea id="message" name="message"></textarea>
+            <textarea id="message" name="message" required></textarea>
           </div>
+
+          <div className={submitted ? "form-group" : "d-none"}>
+            <div className="alert">{responseMessage}</div>
+          </div>
+
           <div className="form-group">
-            <button type="submit" className="btn btn-primary">
+            <button
+              disabled={submitted}
+              type="submit"
+              className="btn btn-primary"
+            >
               CONTACT US
             </button>
           </div>
